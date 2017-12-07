6c12e3a1648dc619f16920f96082f397d74e0127
Jaeger <hj.q@qq.com>
2017-12-07 10:13:37 +0800

    remove create_function()

--- phpQuery.php
+++ phpQuery.php
@@ -3,7 +3,7 @@
  * phpQuery is a server-side, chainable, CSS3 selector driven
  * Document Object Model (DOM) API based on jQuery JavaScript Library.
  *
- * @version 0.9.8
+ * @version 0.9.9
  * @link http://code.google.com/p/phpquery/
  * @link http://phpquery-library.blogspot.com/
  * @link http://jquery.com/
